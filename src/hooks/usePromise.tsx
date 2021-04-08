2b3808fc8d2a1503c1edae26245fdac4568e0912
codetunez <codetunez@outlook.com>
2021-04-08 10:49:25 -0700

    Working job submission

---
+++ src/hooks/usePromise.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+const usePromise = () => {
+    const [loading, setLoading] = React.useState(false);
+    const [data, setData] = React.useState<any>(null);
+    const [error, setError] = React.useState(null);
+
+    const callPromise = async ({ promiseFn }: { promiseFn: any }) => {
+        setLoading(true);
+        setData(null);
+        setError(null);
+        try {
+            const res = await promiseFn();
+            setData(res);
+        } catch (error) {
+            setError(error);
+        }
+        setLoading(false);
+    };
+
+    return [loading, data, error, callPromise];
+};
+
+export default usePromise

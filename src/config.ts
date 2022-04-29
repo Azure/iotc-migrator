a158aa6ef0cde35244dfb920e4e759a146c63121
Vincenzo Caruso <vicaruso@microsoft.com>
2022-04-28 19:04:11 -0700

    revert config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 
 export const Config = {
     AADLoginServer: 'https://login.microsoftonline.com',
-    AADClientID: 'db2b0d5f-966d-4ec4-bf8a-7c5048a8a0e1',
-    AADDirectoryID: 'f9ece212-aff6-4e3b-8da7-3e76918a9f93',
+    AADClientID: '<your-AAD-Application-(client)-ID>',
+    AADDirectoryID: '<your-AAD-Directory-(tenant)-ID>',
     AADRedirectURI: 'http://localhost:3000',
-    applicationHost: 'migration-17.azureiotcentral.com'
+    applicationHost: '<your-iot-central-app>.azureiotcentral.com'
 }
